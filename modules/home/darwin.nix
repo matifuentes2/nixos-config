6cae28ddfeb88de7cebf3c6260a7d4ecfc29a0ad
Matías Fuentes <55928941+matifuentes2@users.noreply.github.com>
2026-08-06 17:55:22 -0400

    feat(darwin): enable vi mode in zsh

--- modules/home/darwin.nix
+++ modules/home/darwin.nix
@@ -3,5 +3,10 @@
 {
   # zsh is the standard interactive shell on macOS. Shared shell tools enable
   # their zsh integration in modules/home/common.nix.
-  programs.zsh.enable = true;
+  programs.zsh = {
+    enable = true;
+    initContent = ''
+      bindkey -v
+    '';
+  };
 }
