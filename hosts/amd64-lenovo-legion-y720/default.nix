ba04f157e0eb49c5ec91e6dbfd4c4e6377456b2b
Matías Fuentes <55928941+matifuentes2@users.noreply.github.com>
2026-08-20 21:45:49 +0000

    Enable Bluetooth and Firefox on Lenovo desktop (#20)
    * Enable Bluetooth on Lenovo desktop
    
    * Add Firefox to Lenovo desktop

--- hosts/amd64-lenovo-legion-y720/default.nix
+++ hosts/amd64-lenovo-legion-y720/default.nix
@@ -31,6 +31,10 @@
   nixpkgs.config.allowUnfree = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
     graphics.enable = true;
     nvidia = {
       # The current stable branch no longer supports Pascal GPUs. Keep this
@@ -65,6 +69,7 @@
 
   environment.systemPackages = with pkgs; [
     efibootmgr
+    firefox
     gh
     git
     vim
@@ -74,6 +79,8 @@
   # Use compressed RAM-backed swap. Disk-backed swap and hibernation are
   # intentionally omitted from the initial installation.
   zramSwap.enable = true;
+
+  services.blueman.enable = true;
 
   services.tailscale = {
     enable = true;
