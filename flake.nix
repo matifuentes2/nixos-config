7320a7ca976523a1ef5adfd781f8b531b4e687ad
Matías Fuentes <55928941+matifuentes2@users.noreply.github.com>
2026-08-23 22:21:53 +0000

    chore: upgrade Orca to 1.4.188

--- flake.nix
+++ flake.nix
@@ -63,7 +63,7 @@
       flake = false;
     };
     orca = {
-      url = "github:stablyai/orca/v1.4.185";
+      url = "github:stablyai/orca/v1.4.188";
       flake = false;
     };
     sops-nix = {
